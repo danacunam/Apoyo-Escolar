3929e23e669a54b6f9667ed9dfd448a12b3e02e1
Daniel  Acuña <DanJ@MacBook-Pro-de-Daniel.local>
2021-07-01 19:19:47 -0300

    agrego scripts

---
+++ script/scripts.js
@@ -0,0 +1,19 @@
+
+
+
+let header = document.getElementById('header');
+
+
+ 
+// HEADER 
+
+function headerChange() {
+    if (window.pageYOffset >= header.offsetTop+100) {
+        header.classList.replace('header-init','header-fixed');
+        }else{
+            header.classList.replace('header-fixed','header-init')
+            } 
+    }
+
+
+window.addEventListener('scroll', headerChange);
