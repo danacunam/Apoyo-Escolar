a52d6bff73888c97b6b4756dd52a386eca190509
Daniel  Acuña <DanJ@MacBook-Pro-de-Daniel.local>
2021-07-03 14:30:48 -0300

    Animaciones y carousel

--- script/script2.js
+++
@@ -1,9 +0,0 @@
-let hc = document.getElementById('hc');
-function hcChange(){
-    if ( window.pageYOffset >= hc.offsetTop+100 ) {
-        hc.classList.replace('header-fixed-c','header-fixed');
-        }else{
-         hc.classList.replace('header-fixed','header-fixed-c')
-            } 
-}
-window.addEventListener('scroll', hcChange);
