270fb93629465f270131d9c2d5c43b83029fe149
Daniel  Acuña <DanJ@MacBook-Pro-de-Daniel.local>
2021-07-04 15:13:50 -0300

    menu mobile y carousel off

--- script/scripts.js
+++ script/scripts.js
@@ -20,4 +20,21 @@ function headerChange() {
 
 window.addEventListener('scroll', headerChange);
 
+//
+let navContent = document.getElementById("navMovil");
+let openButton = document.getElementById("open-btn");
+let closeButton = document.getElementById("close-btn");
 
+function openNav() {
+    navContent.classList.add('mobile-nav-inactive');
+    navContent.classList.replace('mobile-nav-inactive','mobile-nav-active');
+    openButton.classList.replace('btn-movil-o-active','btn-movil-o');
+    closeButton.classList.replace('btn-movil-c','btn-movil-c-active');
+  }
+  
+  function closeNav() {
+    navContent.classList.replace('mobile-nav-active','mobile-nav-inactive');
+    openButton.classList.replace('btn-movil-o','btn-movil-o-active');
+    closeButton.classList.replace('btn-movil-c-active','btn-movil-c');
+    
+  }
