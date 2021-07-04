94504b4f6c18c914b5e199cf862c21d5016a694e
Daniel  Acuña <DanJ@MacBook-Pro-de-Daniel.local>
2021-07-04 18:20:31 -0300

    wa button animado

--- script/scripts.js
+++ script/scripts.js
@@ -24,17 +24,32 @@ window.addEventListener('scroll', headerChange);
 let navContent = document.getElementById("navMovil");
 let openButton = document.getElementById("open-btn");
 let closeButton = document.getElementById("close-btn");
+let wa=document.getElementById("wa-btn");
+let navBtn = document.getElementsByClassName("nav-link");
+let ccount = 0;
 
 function openNav() {
-    navContent.classList.add('mobile-nav-inactive');
-    navContent.classList.replace('mobile-nav-inactive','mobile-nav-active');
+    ccount=ccount+1
+    if(ccount<=1){
     openButton.classList.replace('btn-movil-o-active','btn-movil-o');
+    navContent.classList.add('mobile-nav-active');
     closeButton.classList.replace('btn-movil-c','btn-movil-c-active');
+    wa.classList.add("wa-button-a");
+  }
+  else{
+    openButton.classList.replace('btn-movil-o-active','btn-movil-o');
+    navContent.classList.replace('mobile-nav-inactive','mobile-nav-active');
+    closeButton.classList.replace('btn-movil-c','btn-movil-c-active');
+    wa.classList.replace("wa-button-b","wa-button-a");
+   }
   }
   
   function closeNav() {
+    
     navContent.classList.replace('mobile-nav-active','mobile-nav-inactive');
     openButton.classList.replace('btn-movil-o','btn-movil-o-active');
     closeButton.classList.replace('btn-movil-c-active','btn-movil-c');
+    wa.classList.replace("wa-button-a","wa-button-b");
     
   }
+ 
