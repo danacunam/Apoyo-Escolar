c048818a5efa68f8d5e96f4aac9187357dca4aca
Daniel  Acuña <DanJ@MacBook-Pro-de-Daniel.local>
2021-07-14 23:09:08 -0300

    correccion de página cursos y breakpoints tablet

--- script/scripts.js
+++ script/scripts.js
@@ -7,6 +7,7 @@ function headerChange() {
         header.classList.replace('header-init','header-fixed');  
         }else header.classList.replace('header-fixed','header-init');
        }
+       
 addEventListener('scroll', headerChange);
 
 
